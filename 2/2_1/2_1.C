16cace381860ec5c9bb339c22b4760c87ee2e02c
Nort-Fog <severfog5690jp@gmail.com>
2023-09-19 23:31:12 +0300

    da

--- 2/2_1/2_1.C
+++ 2/2_1/2_1.C
@@ -11,6 +11,9 @@ void main()
     printf("Введите результаты экзаменов = \n");
     scanf("%d%d%d", &a,&b,&c);
 
+
+    int a_as_int = (int)'a';
+    printf("%d",a_as_int);
     d=(a+b+c)/3;
     if (d>4)
         printf("Ученик отличник ");
